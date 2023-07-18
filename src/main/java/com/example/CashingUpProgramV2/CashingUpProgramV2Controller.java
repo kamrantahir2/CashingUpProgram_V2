d294d65418c2046c2f05c38def159f1d654c9de3
kamrantahir2 <59852029+kamrantahir2@users.noreply.github.com>
2023-07-18 14:29:30 +0100

    Add Textfield, label and button for Twenty P coins in Scenebuilder and create method for button to create an instance of Money class and set label to the sum of class

--- src/main/java/com/example/CashingUpProgramV2/CashingUpProgramV2Controller.java
+++ src/main/java/com/example/CashingUpProgramV2/CashingUpProgramV2Controller.java
@@ -1,17 +1,31 @@
 package com.example.CashingUpProgramV2;
 
+import com.example.CashingUpProgramV2.model.Money;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class CashingUpProgramV2Controller {
-    @FXML
-    private Label label;
-    @FXML
-    private Button button;
+    private Money twentyP;
 
-    public void buttonPressed() {
-        label.setText("BUTTON PRESSED!!!!");
+    @FXML
+    private Button twentyPButton;
+    @FXML
+    private Label twentyPLabel;
+    @FXML
+    private TextField twentyPTextField;
+
+    public void calculateTwentyP() {
+        int quantity = Integer.parseInt(twentyPTextField.getText());
+        twentyP = new Money(quantity, 0.2);
+        twentyPLabel.setText("£0.20 = £" + formatSum(twentyP.getSum()));
+    }
+
+
+    public String formatSum(double sum) {
+        return String.format("%.2f", sum);
     }
 
 }
+
